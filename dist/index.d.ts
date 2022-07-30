ea30fc5dec253a34ada0fbc29faf59c3ebe27aff
Semih Ayhan <smhayhan@gmail.com>
2022-07-30 12:56:53 +0300

    wip

---
+++ dist/index.d.ts
@@ -0,0 +1,11 @@
+import { fabric } from 'fabric';
+import { useFabricJSEditor, FabricJSEditor, FabricJSEditorHook } from './editor';
+export interface Props {
+    className?: string;
+    onReady?: (canvas: fabric.Canvas) => void;
+}
+/**
+ * Fabric canvas as component
+ */
+declare const FabricJSCanvas: ({ className, onReady }: Props) => JSX.Element;
+export { FabricJSEditor, FabricJSCanvas, FabricJSEditorHook, useFabricJSEditor };
